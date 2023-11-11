c3d21583d2a601b690cb68cac33026b3653a8c25
fubits <ilja_s@protonmail.com>
2023-11-11 20:06:53 +0100

    init example

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  /** import {MyImage} from "../assets/im3rd-media-wDJ80Pl66mg-unsplash-2000.jpg"
+   * throws a 500 error
+   */
+
+  /* this works */
+  import MyImage from '../assets/im3rd-media-wDJ80Pl66mg-unsplash-2000.jpg?enhanced';
+</script>
+
+<h1>Debug: `enhanced-img` import</h1>
+
+<enhanced:img src={MyImage} alt="" />
+
+<style>
+  :global(img) {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
